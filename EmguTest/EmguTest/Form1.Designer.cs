a2aedeec6e4db1bf3083f8e4a9a7e45e85a603b7
DEAN-HOME\柏閔 <dean10517@gmail.com>
2021-10-08 23:12:23 +0800

    [初始化] 框選時Mat.GetData會有null的問題

---
+++ EmguTest/EmguTest/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace EmguTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic_Result = new System.Windows.Forms.PictureBox();
+            this.pic_Template = new System.Windows.Forms.PictureBox();
+            this.cmb_Method = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chk_Stream_On = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Result)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Template)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_Result
+            // 
+            this.pic_Result.Location = new System.Drawing.Point(253, 12);
+            this.pic_Result.Name = "pic_Result";
+            this.pic_Result.Size = new System.Drawing.Size(600, 651);
+            this.pic_Result.TabIndex = 1;
+            this.pic_Result.TabStop = false;
+            this.pic_Result.Paint += new System.Windows.Forms.PaintEventHandler(this.pic_Result_Paint);
+            this.pic_Result.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pic_Result_MouseDown);
+            this.pic_Result.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pic_Result_MouseMove);
+            this.pic_Result.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pic_Result_MouseUp);
+            // 
+            // pic_Template
+            // 
+            this.pic_Template.Location = new System.Drawing.Point(859, 12);
+            this.pic_Template.Name = "pic_Template";
+            this.pic_Template.Size = new System.Drawing.Size(227, 190);
+            this.pic_Template.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pic_Template.TabIndex = 2;
+            this.pic_Template.TabStop = false;
+            // 
+            // cmb_Method
+            // 
+            this.cmb_Method.FormattingEnabled = true;
+            this.cmb_Method.Items.AddRange(new object[] {
+            "Sqdiff",
+            "SqdiffNormed",
+            "Ccorr",
+            "CcorrNormed",
+            "Ccoeff",
+            "CcoeffNormed"});
+            this.cmb_Method.Location = new System.Drawing.Point(12, 13);
+            this.cmb_Method.Name = "cmb_Method";
+            this.cmb_Method.Size = new System.Drawing.Size(121, 20);
+            this.cmb_Method.TabIndex = 3;
+            this.cmb_Method.SelectedIndexChanged += new System.EventHandler(this.cmb_Method_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1011, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1129, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chk_Stream_On
+            // 
+            this.chk_Stream_On.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chk_Stream_On.AutoSize = true;
+            this.chk_Stream_On.Location = new System.Drawing.Point(98, 56);
+            this.chk_Stream_On.Name = "chk_Stream_On";
+            this.chk_Stream_On.Size = new System.Drawing.Size(35, 22);
+            this.chk_Stream_On.TabIndex = 6;
+            this.chk_Stream_On.Text = "OFF";
+            this.chk_Stream_On.UseVisualStyleBackColor = true;
+            this.chk_Stream_On.CheckedChanged += new System.EventHandler(this.chk_Stream_On_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1469, 675);
+            this.Controls.Add(this.chk_Stream_On);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmb_Method);
+            this.Controls.Add(this.pic_Template);
+            this.Controls.Add(this.pic_Result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Result)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Template)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pic_Result;
+        private System.Windows.Forms.PictureBox pic_Template;
+        private System.Windows.Forms.ComboBox cmb_Method;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox chk_Stream_On;
+    }
+}
+
---
+++ EmguTest/EmguTest/Form1.cs
@@ -0,0 +1,211 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace EmguTest
+{
+    public partial class Form1 : Form
+    {
+        string imgSrcPath = "C:\\Users\\柏閔\\Desktop\\S__33857542.jpeg";
+        string templSrc = "C:\\Users\\柏閔\\Desktop\\S__33857542_template.jpg";
+
+        private VideoCapture usbCam;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            usbCam = new VideoCapture(0);
+            usbCam.ImageGrabbed += UsbCam_ImageGrabbed;
+        }
+
+      
+        private void UsbCam_ImageGrabbed(object sender, EventArgs e)
+        {     
+            //取得畫面
+            Mat frame = new Mat();
+            usbCam.Retrieve(frame);
+
+            //畫十字線
+            Point p1 = new Point(0, frame.Height / 2);
+            Point p2 = new Point(frame.Width, frame.Height / 2);
+            Point p3 = new Point(frame.Width / 2, 0);
+            Point p4 = new Point(frame.Width / 2, frame.Height);
+            CvInvoke.Line(frame, p1, p2, new MCvScalar(0, 255, 0, 0));
+            CvInvoke.Line(frame, p3, p4, new MCvScalar(0, 255, 0, 0));
+
+            //將加工後的畫面輸出到pictureBox上
+            pic_Result.Image = frame.ToBitmap();
+
+            //此延遲影響畫面更新率
+            Thread.Sleep(100);
+        }
+
+        private void cmb_Method_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Emgu.CV.CvEnum.TemplateMatchingType matchingType = Emgu.CV.CvEnum.TemplateMatchingType.Ccoeff;
+            switch (cmb_Method.SelectedIndex)
+            {
+                case 0:
+                    matchingType = Emgu.CV.CvEnum.TemplateMatchingType.Ccoeff;
+                    break;
+
+                case 1:
+                    matchingType = Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed;
+                    break;
+
+                case 2:
+                    matchingType = Emgu.CV.CvEnum.TemplateMatchingType.Ccorr;
+                    break;
+
+                case 3:
+                    matchingType = Emgu.CV.CvEnum.TemplateMatchingType.CcorrNormed;
+                    break;
+
+                case 4:
+                    matchingType = Emgu.CV.CvEnum.TemplateMatchingType.Sqdiff;
+                    break;
+
+                case 5:
+                    matchingType = Emgu.CV.CvEnum.TemplateMatchingType.SqdiffNormed;
+                    break;
+
+            }
+
+            Mat matSrc = new Mat(imgSrcPath);
+            Mat matTemplate = new Mat(templSrc);
+            Mat imgout = new Mat();
+
+            // To resize the image             
+            Size newSize1 = new Size(matSrc.Size.Width / 10, matSrc.Size.Height / 10);
+            Size newSize2 = new Size(matTemplate.Size.Width / 10, matTemplate.Size.Height / 10);
+            CvInvoke.Resize(matSrc, matSrc, newSize1);
+            CvInvoke.Resize(matTemplate, matTemplate, newSize2);
+            CvInvoke.MatchTemplate(matSrc, matTemplate, imgout, matchingType);
+
+
+            double minVal = 0.0;
+            double maxVal = 0.0;
+            Point minLoc = new Point();
+            Point maxLoc = new Point();
+
+            pic_Template.Image = imgout.ToBitmap();
+            CvInvoke.MinMaxLoc(imgout, ref minVal, ref maxVal, ref minLoc, ref maxLoc);
+
+            Console.WriteLine(minVal + "," + maxVal);
+
+            Rectangle r = new Rectangle(maxLoc, matTemplate.Size);
+            CvInvoke.Rectangle(matSrc, r, new MCvScalar(255, 0, 0), 3);
+
+            Rectangle r2 = new Rectangle(minLoc, matTemplate.Size);
+            CvInvoke.Rectangle(matSrc, r2, new MCvScalar(0, 255, 0), 1);
+
+            pic_Result.Image = matSrc.ToBitmap();
+        }
+       
+
+        Point startPos;      // mouse-down position
+        Point currentPos;    // current mouse position
+        bool drawing;        // busy drawing
+        private Rectangle getRectangle()
+        {
+            return new Rectangle(
+                Math.Min(startPos.X, currentPos.X),
+                Math.Min(startPos.Y, currentPos.Y),
+                Math.Abs(startPos.X - currentPos.X),
+                Math.Abs(startPos.Y - currentPos.Y));
+        }
+        private void pic_Result_MouseDown(object sender, MouseEventArgs e)
+        {
+            currentPos = startPos = e.Location;
+            drawing = true;
+        }
+        private void pic_Result_MouseMove(object sender, MouseEventArgs e)
+        {
+            PictureBox picBox = (PictureBox)sender;
+            currentPos = e.Location;
+            if (drawing) picBox.Invalidate();            
+        }
+        private void pic_Result_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                drawing = false;
+                var rc = getRectangle();
+                (sender as PictureBox).Invalidate();
+            }
+        }
+        private void pic_Result_Paint(object sender, PaintEventArgs e)
+        {
+            //Mat matSrc =  pic_Result.Image;
+            
+
+
+            if (drawing)
+            {
+                Mat matSrc = GetMatFromSDImage(pic_Result.Image);
+                matSrc = new Mat(matSrc, getRectangle());
+                //if (matSrc.GetData() != null)
+                pic_Template.Image = matSrc.ToBitmap();
+                e.Graphics.DrawRectangle(Pens.Red, getRectangle());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            usbCam.Grab();
+            //usbCam.
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        
+
+        private void chk_Stream_On_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            if (checkBox.Checked)
+            {
+                usbCam.Start();
+                checkBox.Text = "ON";
+            }
+            else
+            {
+                usbCam.Stop();
+                checkBox.Text = "OFF";
+            }
+        }
+
+        private Mat GetMatFromSDImage(System.Drawing.Image image)
+        {
+            int stride = 0;
+            Bitmap bmp = new Bitmap(image);
+
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData = bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+
+            System.Drawing.Imaging.PixelFormat pf = bmp.PixelFormat;
+            if (pf == System.Drawing.Imaging.PixelFormat.Format32bppArgb)
+            {
+                stride = bmp.Width * 4;
+            }
+            else
+            {
+                stride = bmp.Width * 3;
+            }
+
+            Image<Bgra, byte> cvImage = new Image<Bgra, byte>(bmp.Width, bmp.Height, stride, (IntPtr)bmpData.Scan0);
+
+            bmp.UnlockBits(bmpData);
+
+            return cvImage.Mat;
+        }
+    }
+}
